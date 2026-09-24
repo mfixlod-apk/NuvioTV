ee8d7f7772a7caca78a2883bc909a771e311510a
mfixlod-apk <mfixlod@gmail.com>
2026-09-24 22:58:29 +0300

    Use unique Telegram TV application ID for install compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,7 @@ android {
     ndkVersion = "29.0.14206865"
 
     defaultConfig {
-        applicationId = "com.nuvio.tv"
+        applicationId = "com.nuvio.tv.telegram"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 24
         targetSdk = 36
